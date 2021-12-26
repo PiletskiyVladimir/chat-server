3153aef1ea29c4b61e3588d299e8357bf39a27e1
piletskiy <piletskiy10@gmail.com>
2021-12-26 18:44:18 +0200

    (V2) setup in progress

---
+++ index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from 'cors';
+import {PORT} from './settings.json';
+
+const app = express();
+
+app.use(cors());
+
+app.listen(PORT, ():void => {
+    console.log(`App is listening on port ${PORT}`);
+});
